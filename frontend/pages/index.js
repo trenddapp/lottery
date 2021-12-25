5a683c0a1dbb8358c15f36a0586d78a5bf6e96d4
parsaakbari1209 <akbariparsa1209@gmail.com>
2021-12-25 18:29:41 +0330

    feat(frontend): initialize frontend using nextjs and tailwindcss

---
+++ frontend/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Lottery | dapp-z</title>
+        <meta name="description" content="Decentralized lottery app by dapp-z." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
