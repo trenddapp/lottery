67afda1557a64d286096f9f361c5b719fbf9f459
parsaakbari1209 <akbariparsa1209@gmail.com>
2022-02-02 22:17:51 +0330

    chore: clean-up sub-packages

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,13 +1,18 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-export default function Home() {
+const Home = () => {
   return (
     <div>
       <Head>
         <title>Lottery | dapp-z</title>
-        <meta name="description" content="Decentralized lottery app by dapp-z." />
+        <meta
+          name="description"
+          content="Decentralized lottery app by dapp-z."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
