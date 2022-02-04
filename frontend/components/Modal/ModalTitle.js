8ec74921e7791f014b608d865908b0df38604bbe
parsaakbari1209 <akbariparsa1209@gmail.com>
2022-02-04 10:49:55 +0330

    feat(frontend): add Modal component

---
+++ frontend/components/Modal/ModalTitle.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const ModalTitle = styled.h3`
+  color: ${({ theme }) => theme.colors.headline};
+  font-weight: bold;
+`;
+
+export default ModalTitle;
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,13 @@
 import "../styles/globals.css";
+import { ThemeProvider } from "styled-components";
+import { light } from "../theme";
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={light}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default MyApp;
