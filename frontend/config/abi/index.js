b85b2ac7a7a78c74ee7a2f58ba9c5a1dade4715c
parsaakbari1209 <akbariparsa1209@gmail.com>
2022-02-06 19:44:16 +0330

    feat(frontend): integrate web3-react

---
+++ frontend/config/abi/index.js
@@ -0,0 +1,1 @@
+export { default as abiLottery } from "./lottery.json";
---
+++ frontend/config/constants/index.js
@@ -0,0 +1,6 @@
+import addresses from "./addresses";
+
+export const defaultChainId = "42";
+export const defaultRpcUrl =
+  "https://kovan.infura.io/v3/5f06f41cca9140d78bdaeef7e07c9d94";
+export const addressLottery = addresses.lottery;
