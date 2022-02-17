33c6d2eec4fa98127015bd5156bb1b026d9c57f4
Mobin <67857772+MobinHajizadeh@users.noreply.github.com>
2022-02-17 16:11:07 +0330

    feat(blockchain): update scripts

--- blockchain/scripts/deploy.py
+++ blockchain/scripts/deploy.py
@@ -1,33 +1,49 @@
-from brownie import Lottery, config, network
-from scripts.useful import get_account
+from brownie import (
+    Lottery,
+    LotteryProxyAdmin,
+    LotteryProxy,
+)
+from scripts.useful import get_account, encode_function_data
 
-OWNER = get_account()
-
-LINK_TOKEN = config["networks"][network.show_active()]["link_token"]
-VRF_COORDINATOR = config["networks"][network.show_active()]["vrf_coordinator"]
-KEYHASH = config["networks"][network.show_active()]["keyhash"]
-FEE = config["networks"][network.show_active()]["keyhash"]
-WINNER_PERCENTAGE = 90
+ACCOUNT = get_account()
 
 
 def deploy_lottery():
-    print(network.show_active())
-    dappz_lottery = Lottery.deploy(
-        # LINK_TOKEN,
-        # VRF_COORDINATOR,
-        # KEYHASH,
-        # FEE,
-        # WINNER_PERCENTAGE,
-        {"from": OWNER},
+    upgradeable_dappz_lottery = Lottery.deploy(
+        {"from": ACCOUNT},
         publish_source=True,
     )
-    return dappz_lottery
+    return upgradeable_dappz_lottery
+
+
+def deploy_proxy_admin():
+    proxy_admin = LotteryProxyAdmin.deploy(
+        {"from": ACCOUNT},
+        publish_source=True,
+    )
+    return proxy_admin
+
+
+def deploy_proxy(lottery, proxy_admin):
+    encoded_initializer_function = encode_function_data()
+    proxy = LotteryProxy.deploy(
+        lottery,
+        proxy_admin,
+        encoded_initializer_function,
+        {"from": ACCOUNT},
+        publish_source=True,
+    )
+    return proxy
 
 
 def main():
-    deploy_lottery()
+    lottery = deploy_lottery()
+    print(f"Lottery deployed to {lottery} !")
+    lottery.initialize({"from": ACCOUNT})
 
+    proxy_admin = deploy_proxy_admin()
+    print(f"Proxy admin deployed to {proxy_admin} !")
 
-# def publish_source(con):
-#     contract = con.at(con[-1])
-#     con.publish_source(contract)
+    proxy = deploy_proxy(lottery, proxy_admin)
+    print(f"Proxy deployed to {proxy} !")
+    proxy.initialize({"from": ACCOUNT})
--- blockchain/scripts/useful.py
+++ blockchain/scripts/useful.py
@@ -7,3 +7,59 @@ def get_account(index=0):
     elif network.show_active() in config["networks"]:
         return accounts.add(config["wallets"]["from_key"])
     return None
+
+
+def encode_function_data(initializer=None, *args):
+    """Encodes the function call so we can work with an initializer.
+
+    Args:
+        initializer ([brownie.network.contract.ContractTx], optional):
+        The initializer function we want to call. Example: `box.store`.
+        Defaults to None.
+
+        args (Any, optional):
+        The arguments to pass to the initializer function
+
+    Returns:
+        [bytes]: Return the encoded bytes.
+    """
+    if not len(args):
+        args = b""
+
+    if initializer:
+        return initializer.encode_input(*args)
+
+    return b""
+
+
+def upgrade(
+    account,
+    proxy,
+    newimplementation_address,
+    proxy_admin_contract=None,
+    initializer=None,
+    *args
+):
+    transaction = None
+    if proxy_admin_contract:
+        if initializer:
+            encoded_function_call = encode_function_data(initializer, *args)
+            transaction = proxy_admin_contract.upgradeAndCall(
+                proxy.address,
+                newimplementation_address,
+                encoded_function_call,
+                {"from": account},
+            )
+        else:
+            transaction = proxy_admin_contract.upgrade(
+                proxy.address, newimplementation_address, {"from": account}
+            )
+    else:
+        if initializer:
+            encoded_function_call = encode_function_data(initializer, *args)
+            transaction = proxy.upgradeToAndCall(
+                newimplementation_address, encoded_function_call, {"from": account}
+            )
+        else:
+            transaction = proxy.upgradeTo(newimplementation_address, {"from": account})
+    return transaction
