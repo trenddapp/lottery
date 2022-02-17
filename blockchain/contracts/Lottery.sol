c7784eea6c97e28d29685d067ff3351dff49fa93
Mobin <67857772+MobinHajizadeh@users.noreply.github.com>
2022-02-17 16:07:48 +0330

    feat(blockchain): update lottery contract to adapt to the VRFConsumerBaseV2

--- blockchain/contracts/Lottery.sol
+++ blockchain/contracts/Lottery.sol
@@ -1,26 +1,41 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
+import "@openzeppelin/openzeppelin-contracts-upgradeable@4.5.0/contracts/access/OwnableUpgradeable.sol";
+import "@openzeppelin/openzeppelin-contracts-upgradeable@4.5.0/contracts/proxy/utils/Initializable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/dev/VRFConsumerBaseV2.sol";
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+contract LotteryUpgradeable is
+    Initializable,
+    OwnableUpgradeable,
+    VRFConsumerBaseV2(0x6168499c0cFfCaCD319c818142124B7A15E857ab)
+{
+    // Chainlink VRFConsumerBaseV2
+    VRFCoordinatorV2Interface constant COORDINATOR =
+        VRFCoordinatorV2Interface(0x6168499c0cFfCaCD319c818142124B7A15E857ab);
+    LinkTokenInterface constant LINKTOKEN =
+        LinkTokenInterface(0x01BE23585060835E02B77ef475b0Cc51aA1e0709);
+    bytes32 constant keyHash =
+        0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+    uint64 constant subscriptionId = 247; // https://vrf.chain.link/
+    uint32 constant callbackGasLimit = 1000000;
+    uint32 constant numWords = 1;
+    uint16 constant requestConfirmations = 3;
+    uint256[] randomWords;
+    uint256 requestId;
 
-contract Lottery is Ownable, VRFConsumerBase {
-    uint256 public lotteryID = 0;
+    uint256 public lotteryID;
     address payable[] public participants;
     uint256 public costPerTicket;
     uint256 public prizePool;
-    uint256 public startingTimestamp;
-    uint256 public closingTimestamp;
+    uint256 startingTimestamp;
+    uint256 closingTimestamp;
     address payable public winner;
     uint256 public randomResult;
-    uint256 public winnerIndex;
-    bool public randomGenerated = false;
-
-    uint8 public winnerPercentage; // % of the prizePool is for the winner
-    // oracle
-    bytes32 internal keyHash;
-    uint256 internal fee;
+    uint256 lotteryDuration;
+    uint8 winnerPercentage;
 
     // represents the status of the lottery
     enum Status {
@@ -31,35 +46,30 @@ contract Lottery is Ownable, VRFConsumerBase {
     }
     Status public lotteryStatus = Status.NOT_STARTED;
 
-    // all the needed information about a lottery
-    struct LotteryInfo {
-        uint256 lotteryID; // ID for lottery
-        address payable[] participants; // players
-        uint256 prizePool; // the amount of ETH for prize money
-        uint256 costPerTicket; // cost per ticket in ETH
-        uint256 startingTimestamp; // block timestamp for start of lottery
-        uint256 closingTimestamp; // block timestamp for end of entries
-        address winner; // winner address
-        uint256 randomNumber; // randomResult
+    modifier canClose() {
+        require(
+            lotteryStatus == Status.OPEN,
+            "You can not close the unstarted lottery!"
+        );
+        require(
+            block.timestamp >= startingTimestamp + lotteryDuration,
+            "Time is not over!"
+        );
+        require(randomResult == 0, "Already closed!"); // to prevent re-closing
+        _;
     }
-    // lottery ID's to info
-    mapping(uint256 => LotteryInfo) public allLotteries;
-
     modifier ifNotStarted() {
         require(lotteryStatus == Status.NOT_STARTED);
         _;
     }
     modifier ifOpen() {
         require(
+            block.timestamp <= startingTimestamp + lotteryDuration,
+            "Time is over!"
+        );
+        require(
             lotteryStatus == Status.OPEN,
             "The lottery has not started yet!"
-        );
-        _;
-    }
-    modifier ifClosed() {
-        require(
-            lotteryStatus == Status.CLOSED,
-            "The lottery has not closed yet!"
         );
         _;
     }
@@ -70,56 +80,37 @@ contract Lottery is Ownable, VRFConsumerBase {
         );
         _;
     }
-
     modifier onlyWinnerOrOwner() {
         require(
             msg.sender == winner || msg.sender == owner(),
-            "Only winner or owner can claim reward!"
+            "Only winner can claim reward!"
         );
         _;
     }
-
     modifier randomNumberGenerated() {
-        require(randomGenerated == true, "The winner has not been selected!");
+        require(winner != address(0), "The winner has not been selected!");
         _;
     }
 
-    event RequestedRandomness(bytes32 requestId);
+    event RequestedRandomWords(uint256 requestId);
     event OpenedLottery(uint256 lotteryId);
     event ClosedLottery(uint256 lotteryId);
+    event CompletedLottery(uint256 lotteryId);
     event ClaimedReward(uint256 lotteryId);
 
-    // constructor(
-    //     address _linkToken,
-    //     address _vrfCoordinator,
-    //     bytes32 _keyHash,
-    //     uint256 _fee,
-    //     uint8 _winnerPercentage
-    // ) public VRFConsumerBase(_vrfCoordinator, _linkToken) {
-    //     keyHash = _keyHash;
-    //     fee = _fee; // varies by network
-    //     winnerPercentage = _winnerPercentage;
-    //     lotteryStatus = Status.NOT_STARTED;
-    // }
-
-    constructor()
-        VRFConsumerBase(
-            0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B, // VRF Coordinator
-            0x01BE23585060835E02B77ef475b0Cc51aA1e0709 // LINK Token
-        )
-    {
-        keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
-        fee = 0.1 * 10**18; // 0.1 LINK (Varies by network)
-        winnerPercentage = 85;
-        lotteryStatus = Status.NOT_STARTED;
+    // constructor
+    function initialize() external initializer {
+        __Ownable_init();
     }
 
-    function startLottery(uint256 _ticketPrice)
-        external
-        ifNotStarted
-        onlyOwner
-    {
+    function startLottery(
+        uint256 _ticketPrice,
+        uint8 _winnerPercentage,
+        uint256 _lotteryDuration
+    ) external ifNotStarted onlyOwner {
         costPerTicket = _ticketPrice;
+        winnerPercentage = _winnerPercentage;
+        lotteryDuration = _lotteryDuration;
         lotteryStatus = Status.OPEN;
         startingTimestamp = block.timestamp; // now
         emit OpenedLottery(lotteryID);
@@ -127,45 +118,37 @@ contract Lottery is Ownable, VRFConsumerBase {
 
     function buyTicket() external payable ifOpen {
         require(msg.value >= costPerTicket, "Please enter a valid value!");
-        participants.push(payable(msg.sender)); // add player to list
         prizePool += costPerTicket; // add value to prizePool
+        participants.push(payable(msg.sender)); // add player to list
     }
 
-    function closeLottery() external ifOpen onlyOwner {
+    function closeLottery() external canClose onlyOwner {
+        _requestRandomWords(); // request random number
         lotteryStatus = Status.CLOSED; // pending
-        closingTimestamp = block.timestamp; // closing time
+        closingTimestamp = block.timestamp;
         emit ClosedLottery(lotteryID);
+        emit RequestedRandomWords(requestId);
     }
 
-    function completeLottery() external ifClosed onlyOwner {
-        // request random number
-        bytes32 requestId = getRandomNumber();
-        lotteryStatus = Status.COMPLETED; // winner picked
-        emit RequestedRandomness(requestId);
-    }
-
-    /**
-     * requests randomness
-     */
-    function getRandomNumber() private returns (bytes32 requestId) {
-        require(
-            LINK.balanceOf(address(this)) >= fee,
-            "Not enough LINK - fill contract with faucet"
+    function _requestRandomWords() private onlyOwner {
+        requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
         );
-        return requestRandomness(keyHash, fee);
     }
 
-    /**
-     * callback function used by VRF Coordinator
-     */
-    function fulfillRandomness(bytes32 requestId, uint256 randomness)
-        internal
-        override
-    {
-        randomGenerated = true;
-        randomResult = randomness;
-        winnerIndex = randomness % participants.length;
-        winner = participants[winnerIndex];
+    function fulfillRandomWords(
+        uint256, /* requestId */
+        uint256[] memory _randomWords
+    ) internal override {
+        randomWords = _randomWords;
+        randomResult = randomWords[0];
+        winner = participants[randomResult % participants.length];
+        lotteryStatus = Status.COMPLETED; // winner picked
+        emit CompletedLottery(lotteryID);
     }
 
     function claimReward()
@@ -175,45 +158,32 @@ contract Lottery is Ownable, VRFConsumerBase {
         onlyWinnerOrOwner
     {
         uint256 winnerPrize = prizePool * (winnerPercentage / 100);
-        transferPrize(winnerPrize);
-        lotteryStatus = Status.NOT_STARTED;
-        addLotteryInfoAndReset();
+        _transferPrize(winnerPrize);
+        _reset();
         emit ClaimedReward(lotteryID);
     }
 
-    function transferPrize(uint256 _winnerPrize) private {
+    function _transferPrize(uint256 _winnerPrize) private {
         prizePool -= _winnerPrize;
         winner.transfer(_winnerPrize); // transfer the winnerPrize to the winner
         payable(owner()).transfer(prizePool); // transfer the rest of the prizePool to the owner
     }
 
-    // add lottery informations and reset
-    function addLotteryInfoAndReset() private {
-        LotteryInfo memory lottery = LotteryInfo(
-            lotteryID,
-            participants,
-            prizePool,
-            costPerTicket,
-            startingTimestamp,
-            closingTimestamp,
-            winner,
-            randomResult
-        );
-        allLotteries[lotteryID++] = lottery;
-        // reset
+    function _reset() private {
+        lotteryStatus = Status.NOT_STARTED;
+        lotteryID++;
         participants = new address payable[](0);
         prizePool = 0;
-        randomResult = 0;
+        costPerTicket = 0;
+        startingTimestamp = 0;
+        closingTimestamp = 0;
         winner = payable(address(0));
-        randomGenerated = false;
-    }
-
-    function withdrawLink(uint256 _balance) external onlyOwner {
-        LINK.transfer(owner(), _balance);
+        randomResult = 0;
+        lotteryDuration = 0;
     }
 
     function withdrawEth() external onlyOwner {
         require(prizePool == 0, "prizePool is not empty!");
-        payable(owner()).transfer(address(this).balance);
+        payable(msg.sender).transfer(address(this).balance);
     }
 }
