a76bcccc874ac38c4734cc6911c5d954abe080de
Mobin <67857772+MobinHajizadeh@users.noreply.github.com>
2022-02-17 11:52:49 +0330

    feat(blockchain): add proxy contract

---
+++ blockchain/contracts/LotteryProxyAdmin.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/openzeppelin-contracts@4.5.0/contracts/proxy/transparent/ProxyAdmin.sol";
+
+contract LotteryProxyAdmin is ProxyAdmin {}
