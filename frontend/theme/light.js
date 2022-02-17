965a533754d9fe7703704dba334ac0870c4e671e
parsaakbari1209 <akbariparsa1209@gmail.com>
2022-02-17 17:53:42 +0330

    feat(frontend): update favicon and theme

--- frontend/theme/light.js
+++ frontend/theme/light.js
@@ -1,10 +1,21 @@
 const lightTheme = {
   isDark: false,
+  borderRadiuses: {
+    xs: "",
+    sm: "0.375rem",
+    md: "0.5rem",
+    lg: "",
+    xl: "",
+    xxl: "",
+  },
   colors: {
+    action: "#145C92",
     background: "#FFFFFF",
-    backgroundAlt: "#FAFAFA",
+    backgroundAlt: "#F5F5F5",
+    border: "#CCCCCC",
     headline: "#333333",
     text: "#4E4E4E",
+    error: "#BBOA21",
   },
   modal: {
     colors: {
