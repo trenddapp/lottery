fc84af33e8f1f3339a507bb632239262f5422899
parsaakbari1209 <akbariparsa1209@gmail.com>
2022-02-20 11:22:46 +0330

    feat(frontend): add useInterval hook

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -1,3 +1,4 @@
 export { default as useActiveWeb3React } from './useActiveWeb3React'
 export { default as useContractLottery } from './useContractLottery'
+export { default as useInterval } from './useInterval'
 export { default as useRpcProvider } from './useRpcProvider'
