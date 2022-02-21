1a45a836c4d9124ad216d749eafc35383e7c1668
parsaakbari1209 <akbariparsa1209@gmail.com>
2022-02-21 10:16:56 +0330

    feat(frontend): add the terms section

--- frontend/src/components/Terms/Terms.js
+++ frontend/src/components/Terms/Terms.js
@@ -1,5 +1,25 @@
+import styled from 'styled-components'
+import { Text } from '../Toolkit'
+
+const StyledContainer = styled.section`
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.backgroundAlt};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.border};
+  border-top: 1px solid ${({ theme }) => theme.colors.border};
+  display: flex;
+  height: 50px;
+  justify-content: center;
+  padding: 14px;
+`
+
 const Terms = () => {
-  return <>Terms</>
+  return (
+    <StyledContainer>
+      <Text fontSize="80%" textAlign="center">
+        Copyright © {new Date().getFullYear()} DAPP-Z
+      </Text>
+    </StyledContainer>
+  )
 }
 
 export default Terms
