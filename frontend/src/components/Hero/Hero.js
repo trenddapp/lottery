a18cd132fa0a26896bf587a79141496ecd6d2d64
parsaakbari1209 <akbariparsa1209@gmail.com>
2022-02-24 22:33:46 +0330

    feat(frontend): add hero UI

--- frontend/src/components/Hero/Hero.js
+++ frontend/src/components/Hero/Hero.js
@@ -1,5 +1,26 @@
+import styled from 'styled-components'
+import HeroPrize from './HeroPrize'
+import HeroTimer from './HeroTimer'
+import HeroTicket from './HeroTicket'
+
+const StyledContainer = styled.section`
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.background};
+  display: flex;
+  flex-direction: column;
+  height: 550px;
+  justify-content: center;
+  padding: 14px;
+`
+
 const Hero = () => {
-  return <>Hero</>
+  return (
+    <StyledContainer>
+      <HeroPrize />
+      <HeroTimer />
+      <HeroTicket />
+    </StyledContainer>
+  )
 }
 
 export default Hero
