b9f3bbd9405c6f6586bbc7a6b050026f80f53d38
parsaakbari1209 <akbariparsa1209@gmail.com>
2022-02-27 12:25:01 +0330

    feat(frontend): add layout

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,4 +1,18 @@
 import Head from 'next/head'
+import dynamic from 'next/dynamic'
+
+const LotteryStatusProvider = dynamic(
+  () => import('../store/LotteryStatus').then((module) => module.LotteryStatusProvider),
+  { ssr: false },
+)
+
+const Hero = dynamic(() => import('../components/Hero').then((module) => module.Hero), { ssr: false })
+
+const History = dynamic(() => import('../components/History').then((module) => module.History), { ssr: false })
+
+const Rules = dynamic(() => import('../components/Rules').then((module) => module.Rules), { ssr: false })
+
+const Terms = dynamic(() => import('../components/Terms').then((module) => module.Terms), { ssr: false })
 
 const Home = () => {
   return (
@@ -8,6 +22,12 @@ const Home = () => {
         <meta name="description" content="Decentralized Lottery by DAPP-Z" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <LotteryStatusProvider>
+        <Hero />
+        <History />
+        <Rules />
+        <Terms />
+      </LotteryStatusProvider>
     </>
   )
 }
