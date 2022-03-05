75400aef08ee0e44655cf57ac82dee52804a3155
parsaakbari1209 <akbariparsa1209@gmail.com>
2022-03-05 10:42:25 +0330

    refactor(frontend): rework toasts

---
+++ frontend/src/theme/colors.js
@@ -0,0 +1,21 @@
+const baseColors = {
+  failure: '#ED4B9E',
+  secondary: '#7645D9',
+  success: '#31D0AA',
+  warning: '#FFB237',
+}
+
+export const darkColors = {
+  ...baseColors,
+}
+
+export const lightColors = {
+  ...baseColors,
+  action: '#145C92',
+  background: '#FFFFFF',
+  backgroundAlt: '#F5F5F5',
+  border: '#E9E9E9',
+  borderAlt: '#CCCCCC',
+  headline: '#333333',
+  text: '#4E4E4E',
+}
