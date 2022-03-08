1d62b2dd7fee53d126156fb79d9384fafca7342e
parsaakbari1209 <akbariparsa1209@gmail.com>
2022-03-08 19:11:06 +0330

    feat(frontend): add wallet addresses as etherscan links

--- frontend/src/utils/address.js
+++ frontend/src/utils/address.js
@@ -1,3 +1,7 @@
+export const getEtherscanUrl = (address) => {
+  return 'https://rinkeby.etherscan.io/address/' + address
+}
+
 export const shortenAddress = (address) => {
   if (address.length !== 42) {
     return '0x0...000'
