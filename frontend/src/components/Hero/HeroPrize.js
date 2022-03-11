db6c5367a2eed03810f752cab2060b481e33db88
parsaakbari1209 <akbariparsa1209@gmail.com>
2022-03-11 23:38:22 +0330

    feat(frontend): update hero on status change

--- frontend/src/components/Hero/HeroPrize.js
+++ frontend/src/components/Hero/HeroPrize.js
@@ -1,30 +1,54 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { ethers } from 'ethers'
 import { useTheme } from 'styled-components'
+import { LotteryContext } from '../../store/Lottery'
 import { useContractLottery, useInterval } from '../../hooks'
 import { Flex, Text } from '../Toolkit'
 
 const HeroPrize = () => {
+  const { status } = useContext(LotteryContext)
   const [prize, setPrize] = useState(0)
   const contractLottery = useContractLottery()
   const theme = useTheme()
 
   // TODO: Check for status before polling prize pool.
   useInterval(() => {
-    if (contractLottery !== undefined) {
-      contractLottery
-        .prizePool()
-        .then((prizePool) => {
-          // TODO: Fetch conversion rate from backend.
-          const prizeInEth = parseFloat(ethers.utils.formatEther(prizePool))
-          const prizeInUsd = prizeInEth * 2000
-          setPrize(prizeInUsd)
-        })
-        .catch((err) => {
-          console.log(err)
-        })
+    if (contractLottery === undefined) {
+      return
     }
+
+    if (status === 0 || status === 3) {
+      return
+    }
+
+    contractLottery
+      .prizePool()
+      .then((prizePool) => {
+        // TODO: Fetch conversion rate from backend.
+        const prizeInEth = parseFloat(ethers.utils.formatEther(prizePool))
+        const prizeInUsd = prizeInEth * 2000
+        setPrize(prizeInUsd)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }, 20000)
+
+  if (status === 0 || status === 3) {
+    return (
+      <Flex alignItems="center" flexDirection="column" justifyContent="center">
+        <Text color={theme.colors.headline} fontSize="38px">
+          Next{' '}
+          <Text as="span" color={theme.colors.action}>
+            Lottery
+          </Text>
+        </Text>
+        <Text color={theme.colors.headline} fontSize="38px">
+          Starting Soon!
+        </Text>
+      </Flex>
+    )
+  }
 
   return (
     <Flex alignItems="center" flexDirection="column" justifyContent="center">
