d287bca3f1a9d4874de82dcc617704dc89049009
parsaakbari1209 <parsaakbari1209@gmail.com>
2022-03-17 13:01:08 +0330

    WIP

--- backend/service/currency/main.go
+++ backend/service/currency/main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/dapp-z/lottery/backend/service/currency/cmd"
 )
 
 func main() {
 	cmd.Execute()
-}
-
-func unUsedFunction() {
-	fmt.Println("Hello World!")
 }
