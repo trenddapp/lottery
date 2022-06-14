3df4966a21511fc8840681bc467cae5315c084d5
parsaakbari1209 <parsaakbari1209@gmail.com>
2022-06-15 00:33:13 +0430

    chore(frontend): prefix next specific urls

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  assetPrefix: '/auction',
   reactStrictMode: true,
+  rewrites() {
+    return [{ source: '/auction/_next/:path*', destination: '/_next/:path*' }]
+  },
 }
+
+module.exports = nextConfig
